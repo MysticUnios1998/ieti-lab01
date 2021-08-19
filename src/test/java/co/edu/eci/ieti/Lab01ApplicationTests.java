051f8bb1d0040b43458d92a3270bda666a851abb
Eduard Arias <eduard.arias@mail.escuelaing.edu.co>
2021-08-19 19:02:31 -0400

    Inicio de arquitectura para el microservicio de usuario

---
+++ src/test/java/co/edu/eci/ieti/Lab01ApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.eci.ieti;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
